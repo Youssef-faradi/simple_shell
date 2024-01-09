732f776970ca67a703828902d18b6f318f1c3a8e
Youssef-faradi <faradiyoussef1@gmail.com>
2024-01-09 07:02:31 -0800

    task one fix

--- shell.h
+++ shell.h
@@ -7,10 +7,24 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
 
-#define MAX_INPUT_LENGTH 1024
+extern char **environ;
 
 char *prompt();
 char **split(char *input);
+int executor(char **command, char **argv, int index);
+void free2d(char **array);
+char *_getenv(char *str);
+char *pathfinder(char *cmd);
+void error(char *shell, char *cmd, int index);
+void print_environment(void);
+int builtin_exit(char **command);
+int unsetenv_builtin(char **command);
+int setenv_builtin(char **command);
+int change_dir(char **path);
 
 #endif
