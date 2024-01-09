97696c11e2830c74e83ef49f90bc9b754a21d466
Youssef-faradi <faradiyoussef1@gmail.com>
2024-01-09 07:32:48 -0800

    new task one

--- main.c
+++ main.c
@@ -1,40 +1,58 @@
-#include "shell.h"
+#ifndef MAIN
+#define MAIN
+
+/*<-----Standard Libraries----->*/
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+
+/*<------Data Structures------->*/
 
 /**
- * main - Entry point for the simple shell.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the command-line arguments.
- * Return: 0 (Success) 1 (Failure)
+ * struct list_t - singly linked list
+ * @path: a path variable value
+ * @next: points to the next node
+ *
+ * Description: PATH structure list
+ *
  */
-int main(int argc, char **argv)
+typedef struct list_t
 {
-	char **command = NULL;
-	char *line = NULL;
-	int status = 0, index = 0;
-	(void) argc;
+	char *path;
+	struct list_t *next;
+} list_t;
 
-	while (1)
-	{
-		line = prompt();
-		if (line == NULL)
-		{
-			if (isatty(STDIN_FILENO) == 1)
-				write(1, "\n", 1);
-			free(line), line = NULL;
-			return (status);
-		}
-		index++;
-		command = split(line);
-		if (!command)
-			continue;
-		if (strcmp(command[0], "cd") == 0)
-			status = change_dir(command);
-		else if (strcmp(command[0], "setenv") == 0)
-			status = setenv_builtin(command);
-		else if (strcmp(command[0], "unsetenv") == 0)
-			status = unsetenv_builtin(command);
-		else
-			status = executor(command, argv, index);
-	}
-	free(line);
-}
+/**
+ * struct env - Array of env variables
+ * @env_var: an env variable value
+ *
+ * Description: custom Environ structure
+ */
+
+typedef struct env
+{
+	char **env_var;
+} New_env;
+
+extern char **environ;
+/*<---------Strings Standard----------->*/
+ssize_t _getline(char **line, size_t *size, FILE *source);
+int _atoi(char *s);
+char *_strchr(char *s, char c);
+char *_strcat(char *dest, char *src);
+char *_strtok(char *str, const char *delim);
+unsigned int _strcspn(char *s, char *reject);
+int _strncmp(char *s1, char *s2, int num);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
+
