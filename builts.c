190180c68000dfb38807fdb0875acf0b76eed68b
Youssef-faradi <faradiyoussef1@gmail.com>
2024-01-09 17:20:58 +0100

    check

---
+++ builts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _built - Checks the exixstence of builtins
+ *	commands
+ * @token_0: the strings to check
+ * Return: Success or Failure
+ */
+
+int _built(char *token_0)
